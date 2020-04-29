7a923ccbd22694b57b4410a9457d27a91e95d401
aymeric <aymeric.ferreira@gmail.com>
2020-04-29 11:46:48 -0400

    split param and equadiff

--- equadiff.py
+++ equadiff.py
@@ -14,9 +14,10 @@ def chemical_equations(y, t, k):
     f = [
 
     ]
-    return [-rf, -2*rf, rf]
+    return f
 
 
+ABC = 2
 
 y0 = [A0, B0, C]
 k = kf,
@@ -26,46 +27,3 @@ t = np.arange(100)
 
 y = odeint(rhs, y0, t, k)
 
-#### PARAMETERS
-# dp/dt
-k_3K =
-k_5P =
-tau_P = 1 / (k_3K + k_5P)
-Vbar_PLC =
-# c =
-K_PLC =
-eta = k_3K / (k_3K + k_5P)
-K_3K =
-# p =
-
-# dc/dt
-k1 =
-# r =
-K_a =
-K_p =
-k2 =
-# s =
-V_serca =
-K_serca =
-epsilon =
-v0 =
-theta =
-V_pm =
-K_pm =
-
-# ds/dt
-beta =
-
-# dr/dt
-tau_r =
-K_i =
-
-# Initialise state variables
-p =
-c =
-s =
-r =
-# pack the state variables
-w0 = [p, c, s, r]
-# pack the parameters
-p = [tau_P, Vbar_PLC, eta, k_3K, k1, K_a, ]
